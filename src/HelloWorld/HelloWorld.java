f6b1536758941bacfc5ba9f0bbc38716b27deb08
andre1210 <nilanjan@whizmantra.com>
2016-08-06 16:09:02 +0530

    Hello World

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+/**
+ * Created by Andre Neal on 8/6/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
